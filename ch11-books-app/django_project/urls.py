6944042198e2b19fa46ed0d505edf354ab25e4e4
jwc20 <choij2015@gmail.com>
2023-02-27 10:09:06 -0800

    Added books app url to project-level urls.

--- ch11-books-app/django_project/urls.py
+++ ch11-books-app/django_project/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # path("accounts/", include("accounts.urls")),
     path("accounts/", include("allauth.urls")),
     path("", include("pages.urls")),
+    path("books/", include("books.urls")),
 ]
