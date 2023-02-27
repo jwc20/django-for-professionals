076258d22e6a83515819e338cf1e149655519352
jwc20 <choij2015@gmail.com>
2023-02-27 10:14:22 -0800

    Fix

--- ch11-books-app/books/urls.py
+++ ch11-books-app/books/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import BookListView, BookDetailView
+from .views import BookListView
 
 urlpatterns = [
     path("", BookListView.as_view(), name="book-list"),
