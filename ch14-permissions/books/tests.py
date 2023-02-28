431e6a7cea32d75783f73851c370de9e3e91daf5
jwc20 <choij2015@gmail.com>
2023-02-28 06:56:46 -0800

    Added tests for permissions.

--- ch14-permissions/books/tests.py
+++ ch14-permissions/books/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.contrib.auth.models import Permission
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
@@ -9,9 +10,13 @@ class BookTests(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user = get_user_model().objects.create_user(
-            username='reviewuser',
-            email='reviewuser@email.com',
-            password='testpass123',
+            username="reviewuser",
+            email="reviewuser@email.com",
+            password="testpass123",
+        )
+
+        cls.special_permission = Permission.objects.get(
+            codename="special_status",
         )
 
         cls.book = Book.objects.create(
@@ -23,7 +28,7 @@ class BookTests(TestCase):
         cls.review = Review.objects.create(
             book=cls.book,
             author=cls.user,
-            review="This is a great book",
+            review="An excellent review",
         )
 
     def test_book_listing(self):
@@ -37,12 +42,21 @@ class BookTests(TestCase):
         self.assertContains(response, "Harry Potter")
         self.assertTemplateUsed(response, "books/book_list.html")
 
-    def test_book_detail_view(self):
+    def test_book_list_view_for_logged_out_user(self):  # new
+        self.client.logout()
+        response = self.client.get(reverse("book_list"))
+        self.assertEqual(response.status_code, 302)  # new
+        self.assertRedirects(response, "%s?next=/books/" % (reverse("account_login")))
+        response = self.client.get("%s?next=/books/" % (reverse("account_login")))
+        self.assertContains(response, "Log In")
+
+    def test_book_detail_view_with_permissions(self):  # new
+        self.client.login(email="reviewuser@email.com", password="testpass123")
+        self.user.user_permissions.add(self.special_permission)
         response = self.client.get(self.book.get_absolute_url())
         no_response = self.client.get("/books/12345/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "Harry Potter")
-        self.assertContains(response, "This is a great book")
+        self.assertContains(response, "An excellent review")
         self.assertTemplateUsed(response, "books/book_detail.html")
-
