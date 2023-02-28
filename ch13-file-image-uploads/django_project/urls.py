3e6a3e3868b86ff5cb3f9d38356aed852479c004
jwc20 <choij2015@gmail.com>
2023-02-28 04:58:49 -0800

    - Since user-uploaded content is assumed to exist in a production context, to see media items locally we need to update django_project/urls.py to show the files locally.

--- ch13-file-image-uploads/django_project/urls.py
+++ ch13-file-image-uploads/django_project/urls.py
@@ -1,11 +1,16 @@
+from django.conf import settings  
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django admin
     path("admin/", admin.site.urls),
-    # path("accounts/", include("django.contrib.auth.urls")),
-    # path("accounts/", include("accounts.urls")),
+    # User management
     path("accounts/", include("allauth.urls")),
+    # Local apps
     path("", include("pages.urls")),
     path("books/", include("books.urls")),
-]
+] + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)  # new
