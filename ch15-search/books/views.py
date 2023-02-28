4fa68509b85b78f10307e695260368d37dd0d16f
jwc20 <choij2015@gmail.com>
2023-02-28 10:28:03 -0800

    Update books app views to add SearchResultListView.

--- ch15-search/books/views.py
+++ ch15-search/books/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
-    PermissionRequiredMixin,  # new
+    PermissionRequiredMixin,
 )
 from django.views.generic import ListView, DetailView
 
@@ -18,9 +18,15 @@ class BookDetailView(
     LoginRequiredMixin,
     PermissionRequiredMixin,
     DetailView,
-):  # new
+):
     model = Book
     context_object_name = "book"
     template_name = "books/book_detail.html"
     login_url = "account_login"
-    permission_required = "books.special_status"  # new
+    permission_required = "books.special_status"
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = "book_list"
+    template_name = "books/search_results.html"
