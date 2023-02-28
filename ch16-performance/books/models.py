37017aff10a81406fe03f60a21b21807577a14c5
jwc20 <choij2015@gmail.com>
2023-02-28 12:05:42 -0800

    Ignore previous commit, use this to add indexing to model.

--- ch16-performance/books/models.py
+++ ch16-performance/books/models.py
@@ -7,7 +7,6 @@ from django.urls import reverse
 class Book(models.Model):
     id = models.UUIDField(
         primary_key=True,
-        db_index=True,
         default=uuid.uuid4,
         editable=False,
     )
@@ -17,6 +16,9 @@ class Book(models.Model):
     cover = models.ImageField(upload_to="covers/", blank=True)
 
     class Meta:
+        indexes = [
+            models.Index(fields=["id"], name="id_index"),
+        ]
         permissions = [
             ("special_status", "Can read all books"),
         ]
