5153322ece8bebe0bf9d5643fe1b0d350ca598cd
jwc20 <choij2015@gmail.com>
2023-02-28 04:12:19 -0800

    Added Review model in admin panel for books.

--- ch12-reviews-app/books/admin.py
+++ ch12-reviews-app/books/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
+from .models import Book, Review
 
-from .models import Book
+
+class ReviewInline(admin.TabularInline):
+    model = Review
 
 
 class BookAdmin(admin.ModelAdmin):
+    inlines = [ReviewInline]
     list_display = (
         "title",
         "author",
